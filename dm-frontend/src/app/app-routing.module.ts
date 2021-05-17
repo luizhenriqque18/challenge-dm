73dd56d993ae1c3033b2b168058690d5b45eba9b
luizhenriqque18 <luizhenriqque15@gmail.com>
2021-05-17 16:09:09 -0400

    config router category

--- dm-frontend/src/app/app-routing.module.ts
+++ dm-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './pages/category/category.component';
 import { DeviceComponent } from './pages/device/device.component';
 
 const routes: Routes = [
   { path: 'device', component: DeviceComponent },
-  { path: 'category', component: AppComponent }
+  { path: 'category', component: CategoryComponent }
 ];
 
 @NgModule({
--- dm-frontend/src/app/pages/category/category.module.ts
+++ dm-frontend/src/app/pages/category/category.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryService } from './services/category.service';
+import { CategoryComponent } from './category.component';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-
+const routes: Routes = [
+  { 
+    path: 'category',
+    children: [
+      {path: 'list', component: ListComponent },
+      {path: 'create', component: CreateComponent},
+      {path: 'details:id', component: CategoryComponent},
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CategoryComponent,
+    ListComponent,
+    CreateComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   providers: [CategoryService]
 })
