2bf61f6a483638ab786a4dfa005093e21e0b80e2
luizhenriqque18 <luizhenriqque15@gmail.com>
2021-05-17 17:42:50 -0400

    fix service

--- dm-frontend/src/app/pages/category/services/category.service.ts
+++ dm-frontend/src/app/pages/category/services/category.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Category } from 'src/app/model/category.model';
 import { environment } from 'src/environments/environment';
+import { CategoryDto } from './category.dto';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,24 @@ export class CategoryService {
 
   constructor(private http: HttpClient) { }
 
-  public pathUrl = `${environment.url}`;
+  public pathUrl = `${environment.url}/category`;
 
   public findAllUsingGet(): Observable<Category[]> {
-    return this.http.get<Category[]>(`${this.pathUrl}/category`);
+    return this.http.get<Category[]>(`${this.pathUrl}`);
+  }
+
+  public findByIdUsingGet(id: string): Observable<Category> {
+
+    return this.http.get<Category>(`${this.pathUrl}/${id}`);
+  }
+
+  public DeleteByIdUsingDelete(id: string): Observable<Category> {
+
+    return this.http.delete<Category>(`${this.pathUrl}/${id}`);
+  }
+
+  public CreateUsingPost(category: CategoryDto): Observable<Category> {
+
+    return this.http.post<Category>(`${this.pathUrl}`, category);
   }
 }
