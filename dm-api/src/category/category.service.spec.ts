8d9ba33f63dfd65105c5103aa97aab14f5c3e4a6
Luiz <luiz.souza@grupoicts.com.br>
2021-05-18 14:17:58 -0400

    [Backend] fix test category

--- dm-api/src/category/category.service.spec.ts
+++ dm-api/src/category/category.service.spec.ts
@@ -1,12 +1,34 @@
+import { getModelToken } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Category } from './category.model';
 import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dtos/create-category.dto';
+
+const mockCategoryService = {
+  create(dto: CreateCategoryDto) {
+    return null;
+  },
+  findAll() {
+    return this.categoryModel.findAll();
+  },
+  findById(id: string): Promise<Category> {
+    return null;
+  },
+  delete(id: string){
+    return null;
+  }
+};
 
 describe('CategoryService', () => {
   let service: CategoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [CategoryService,
+        {
+          provide: getModelToken(Category),
+          useValue: mockCategoryService,
+        }]
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
