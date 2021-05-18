5c02df8e53209e4163c85c1ecf778c93c28e2cd1
Luiz <luiz.souza@grupoicts.com.br>
2021-05-18 14:15:48 -0400

    [Backend] fix import

--- dm-api/src/category/category.module.ts
+++ dm-api/src/category/category.module.ts
@@ -3,10 +3,12 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Category } from './category.model';
-
+import { getRepositoryToken } from '@nestjs/typeorm';
 @Module({
   imports: [SequelizeModule.forFeature([Category])],
-  providers: [CategoryService],
+  providers: [CategoryService, 
+    
+  ],
   controllers: [CategoryController],
   exports: [CategoryService]
 })
